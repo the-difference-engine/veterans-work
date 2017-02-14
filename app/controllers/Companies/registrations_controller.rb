ecead63e45bdde64da97dbb15d3aa49c66c80b58
Chris Woodside <christopherwoodside@gmail.com>
2017-02-13 19:46:54 -0600

    Add Companies edit fields

--- app/controllers/Companies/registrations_controller.rb
+++ app/controllers/Companies/registrations_controller.rb
@@ -46,9 +46,10 @@ before_action :configure_account_update_params, only: [:update]
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zip_code, :phone,
+      :description, :url])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
