c740b4823af95c5b9536baf3cff9a15084bdcb67
James Gates <gatorjuice@gmail.com>
2017-02-15 19:10:47 -0600

    added a test example and starting factory

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :companies, :controllers => { registrations: 'companies/registrations'}
-  # get '/' => 'pages#hello_world'
-
-  # get '/companies' => 'companies#index'
-  # get '/companies/:id' => 'companies#show'
+  devise_for :companies
+  get '/companies' => 'companies#index'
 end
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
+  describe 'GET #index' do
+    it 'assigns all the companies to @companies' do
+      this_company = create(:company)
+      that_company = create(:company)
+      another_company = create(:company)
+      get :index
+      expect(assigns(:companies)).to eq([this_company, that_company, another_company])
+    end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index.html.erb")
+    end
+  end
 end
