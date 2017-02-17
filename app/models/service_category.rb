fdefea60691b1cb6602af209d78d8f91de860119
James Gates <gatorjuice@gmail.com>
2017-02-16 20:45:07 -0600

    back on track maybe

--- app/models/service_category.rb
+++ app/models/service_category.rb
@@ -1,2 +1,4 @@
 class ServiceCategory < ApplicationRecord
+  has_many :company_services
+  has_many :companies, through: :company_services
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217010332) do
+ActiveRecord::Schema.define(version: 20170217024207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170217010332) do
     t.integer "company_id"
     t.integer "role_id"
     t.index ["company_id", "role_id"], name: "index_companies_roles_on_company_id_and_role_id", using: :btree
+  end
+
+  create_table "company_services", force: :cascade do |t|
+    t.integer  "company_id"
+    t.integer  "service_category_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   create_table "roles", force: :cascade do |t|
