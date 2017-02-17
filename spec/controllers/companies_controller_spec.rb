99a1bfbaa7cab9058bf5d2242f7178108da28d1e
James Gates <gatorjuice@gmail.com>
2017-02-16 18:51:16 -0600

    more controller example

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CompaniesController, type: :controller do
   describe "get #index" do
     it 'assigns all companies to @companies' do
       companies_list = []
-      5.times do 
+      5.times do
         companies_list << create(:company)
       end
       get :index
@@ -15,11 +15,17 @@ RSpec.describe CompaniesController, type: :controller do
     end
   end
 
-
     it "renders the index template" do
       get :index
       expect(response).to render_template("index.html.erb")
     end
   end
 
+  describe 'GET #show' do
+    it 'assigns the company to @company' do
+      company = create(:company)
+      get :show, id: company.id
+      expect(assigns(:company)).to eq(company)
+    end
+  end
 end
