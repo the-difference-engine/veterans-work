d59d6eb9cdfcf1012fd4c534c038e09d1803fe57
Myles Woerner <myleswoerner@gmail.com>
2017-02-16 19:09:20 -0600

    add service_category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210015933) do
+ActiveRecord::Schema.define(version: 20170217010332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20170210015933) do
     t.datetime "updated_at"
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
     t.index ["name"], name: "index_roles_on_name", using: :btree
+  end
+
+  create_table "service_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
---
+++ spec/factories/service_categories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :service_category do
+    name "MyString"
+  end
+end
