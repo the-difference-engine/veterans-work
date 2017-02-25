84caf75a557508bacafd17f37edc9b38a41f186c
James Gates <gatorjuice@gmail.com>
2017-02-25 12:17:50 -0600

    done as best I can for now

--- app/controllers/Companies/registrations_controller.rb
+++ app/controllers/Companies/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Companies::RegistrationsController < Devise::RegistrationsController
+fclass Companies::RegistrationsController < Devise::RegistrationsController
 before_action :configure_sign_up_params, only: [:create]
 before_action :configure_account_update_params, only: [:update]
 
