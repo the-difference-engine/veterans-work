27bf88b03f189831ea54f5bbb36ecf5469de85e8
James Gates <gatorjuice@gmail.com>
2017-02-26 13:49:05 -0600

    tests and stuff

--- app/models/company.rb
+++ app/models/company.rb
@@ -16,7 +16,7 @@ class Company < ApplicationRecord
   end
 
   def eligible_customer_requests
-    CustomerRequest.where(
+    CustomerRequest.where("expires_date >= ?", Date.today()).where(
       service_category_id: service_categories
     ).select {|cr| cr.distance_from([latitude, longitude]) <= service_radius }
   end
