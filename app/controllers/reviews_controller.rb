b60d1bc4322f85a7c6bfb1da96c4368e291ecccd
thaker-nish <thaker.nish@gmail.com>
2017-03-02 20:05:49 -0600

    change customer_reviews.coffee file to reviews.js, make review_params method and update the create method.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,17 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    Review.create(
-      customer_id: current_customer.id,
-      company_id: params[:company_id],
-      body: params[:body],
-      stars: params[:stars]
-      )
+    Review.create(review_params)
     redirect_to "/customers/#{current_customer.id}"
+  end
+
+  private
+
+  def review_params
+    params.permit(
+      :company_id,
+      :stars,
+      :body
+      ).merge(customer_id: current_customer.id)
   end
 end
