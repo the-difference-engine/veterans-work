3020c52f3546f1417e6fc0171405caef4c6245fc
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2017-03-02 19:40:27 -0600

    wrote rspec tests for review model

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  factory :customer_review do
-    customer_id 1
-    company_id 1
-    stars 1
-    body "MyText"
+  factory :review do
+    customer
+    company
+    stars [1..5].sample
+    body Faker::Lorem.paragraph
   end
 end
