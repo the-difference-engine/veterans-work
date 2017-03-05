88d0c2e942b8f1e772350a37a3012f11ee779f3c
James Gates <gatorjuice@gmail.com>
2017-03-05 11:46:12 -0600

    test working

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   validates :customer_id, presence: true
   validates :company_id, presence: true
-  validates :stars, numericality: { 
-    only_integer: true, 
+  validates :stars, numericality: {
+    only_integer: true,
     greater_than_or_equal_to: 1,
     less_than_or_equal_to: 5
   }
