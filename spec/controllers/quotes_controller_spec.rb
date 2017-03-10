d4814b7c7e0475e25aef11c655b64426d3f8d197
AaronOoooo <aaron.o.hall@gmail.com>
2017-03-09 20:51:59 -0600

    RSpec test for quotes controller

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe QuotesController, type: :controller do
+  describe 'GET #new' do
+    it 'renders a new form' do
+      get :new
+      expect(response).to render_template("new.html.erb")
+    end
+  end
+  describe 'POST #create' do
+    it 'creates a and saves a new customer quote to the database' do
+      sign_in create(:company)
+      expect{
+        post :create, params: {
+          quote: attributes_for(:quote)
+        }
+      }.to change(Quote, :count).by(1)
+    end
+  end
+
 
 end
