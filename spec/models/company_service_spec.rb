dd8cd019371f5e7bb823ce37848b956298c98b21
James Gates <gatorjuice@gmail.com>
2017-03-19 20:34:59 -0500

    annotations now, hurray

--- spec/models/company_service_spec.rb
+++ spec/models/company_service_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: company_services
+#
+#  id                  :integer          not null, primary key
+#  company_id          :integer
+#  service_category_id :integer
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe CompanyService, type: :model do
