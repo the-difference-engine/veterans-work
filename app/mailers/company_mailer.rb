5db2fd99b9fe536113df1b882c86868bd0115d70
Eric Biel <eric.j.biel@gmail.com>
2017-03-23 19:24:01 -0500

    Create company mailer for approve/reject purposes

---
+++ app/mailers/company_mailer.rb
@@ -0,0 +1,9 @@
+class CompanyMailer < ApplicationMailer
+  default from: "no-reply@veteranswork.com"
+
+  def decline_email(quote)
+    @quote = params[:quote_id]
+    @company = quote.company
+    mail(to: @company.email), subject: "Your quote was rejected by the customer")
+  end
+end
