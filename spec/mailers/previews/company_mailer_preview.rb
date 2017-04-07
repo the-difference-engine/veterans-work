1ebf1016ccf027f209b7b18738b3e5b47e2e1528
Chris Woodside <christopherwoodside@gmail.com>
2017-04-06 20:18:54 -0500

    Change mailer To format to match guide, change preview mailer actions to match

--- spec/mailers/previews/company_mailer_preview.rb
+++ spec/mailers/previews/company_mailer_preview.rb
@@ -1,10 +1,10 @@
 class CompanyMailerPreview < ActionMailer::Preview
-  def decline_email
+  def decline_email_preview
     quote = Quote.last
     CompanyMailer.decline_email(quote)
   end
 
-  def accept_email
+  def accept_email_preview
     quote = Quote.last
     CompanyMailer.accept_email(quote)
   end
