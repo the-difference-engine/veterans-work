b36f9a6a1e30a55720f43a2ecc9743ede7393291
thaker-nish <thaker.nish@gmail.com>
2017-04-13 12:13:13 -0500

    Fix broken tests, add test to redirect user trying to access another users show page.

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -41,14 +41,25 @@ RSpec.describe CompaniesController, type: :controller do
     end
     it 'renders show page' do
       company = create(:company)
+      sign_in company
       get :show, params: { id: company.id }
       expect(response).to render_template("show.html.erb")
+    end
+    it 'redirects a user trying to access another users show page' do
+      company1 = create(:company,
+      email: "test@gmail.com", id: 20)
+      company = create(:company,
+      email: "example@gmail.com", id: 100)
+      sign_in company
+      get :show, params: { id: 20 }
+      expect(:company).to redirect_to("/")
     end
   end
 
   describe 'GET #edit' do
     before :each do
       @company = create(:company)
+      sign_in @company
     end
     it 'asigns the requested company to @company' do
       get :edit, params: { id: @company.id }
@@ -66,6 +77,7 @@ RSpec.describe CompaniesController, type: :controller do
         name: "old value",
         phone: "1234"
       )
+      sign_in company
       patch :update, params: {
         id: company.id,
         name: "New Value",
@@ -77,6 +89,7 @@ RSpec.describe CompaniesController, type: :controller do
     end
     it 'redirect to the company page' do
       company = create(:company)
+      sign_in company
       patch :update, params: { id: company.id }
       expect(response).to redirect_to("/companies/#{company.id}")
     end
@@ -85,6 +98,7 @@ RSpec.describe CompaniesController, type: :controller do
   describe 'DELETE #destroy' do
     before :each do
       @company = create(:company)
+      sign_in @company
     end
     it 'removes the desired company from the database' do
       expect{
@@ -97,6 +111,3 @@ RSpec.describe CompaniesController, type: :controller do
     end
   end
 end
-
-
-
