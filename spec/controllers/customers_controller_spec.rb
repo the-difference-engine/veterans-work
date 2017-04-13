27d225743fbbea2e9b79cac80c72ced9eb1f7dbf
thaker-nish <thaker.nish@gmail.com>
2017-04-13 12:12:16 -0500

    Fix test.

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe CustomersController, type: :controller do
       get :show, params: { id: customer.id }
       expect(assigns(:customer)).to eq(customer)
     end
-    it 'redirects a user trying to access another users pages' do
+    it 'redirects a user trying to access another users show page' do
       customer1 = create(:customer,
       email: "test@gmail.com", id: 20)
       customer = create(:customer,
