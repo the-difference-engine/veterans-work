7f67dd09c889b6bca1654363e4225c815f729e9a
thaker-nish <thaker.nish@gmail.com>
2017-04-13 11:47:49 -0500

    Fix assigning test, write new test to redirect users trying to access unauthorized pages.

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 RSpec.describe CustomersController, type: :controller do
   describe 'GET #show' do
     it 'assigns the requested customer to @customer' do
-      customer = create(:customer)
+      customer = create(:customer,
+      email: "example@gmail.com")
+      sign_in customer
       get :show, params: { id: customer.id }
       expect(assigns(:customer)).to eq(customer)
+    end
+    it 'redirects a user trying to access another users pages' do
+      customer1 = create(:customer,
+        email: "test@gmail.com", id: 20)
+      customer = create(:customer,
+      email: "example@gmail.com", id: 100)
+      sign_in customer
+      get :show, params: { id: 20 }
+      expect(:customer).to redirect_to("/")
     end
   end
 end
