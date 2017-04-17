3fb81a2147ccc5300e8c265a0e2a038331581851
James Gates <gatorjuice@gmail.com>
2017-04-17 17:48:10 -0500

    handles when a company leaves blank values in a quote price

--- spec/factories/quotes.rb
+++ spec/factories/quotes.rb
@@ -19,10 +19,15 @@ FactoryGirl.define do
   factory :quote do
     customer_request
     company
-    materials_cost_estimate "9.99"
-    labor_cost_estimate "9.99"
-    start_date "MyString"
-    completion_date_estimate "MyString"
+    materials_cost_estimate 100.00
+    labor_cost_estimate 100.00
+    start_date Date.today()
+    completion_date_estimate Date.today() + 3
     notes "MyText"
+  end
+
+  trait :blank_costs do
+    materials_cost_estimate ''
+    labor_cost_estimate nil
   end
 end
