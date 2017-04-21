ee56992935340e1180e67225c35d11f98f9ec91f
Chris Woodside <christopherwoodside@gmail.com>
2017-04-20 20:03:56 -0500

    Updated Contracts controller and Contracts controller RSpec tests

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,23 +1,24 @@
 class ContractsController < ApplicationController
 
   def create
-    @accepted_quote = Quote.find(params[:id])
-    @customer_request = @accepted_quote.customer_request
-    @accepted_quote.update(accepted: true)
-    if Contract.create(
-      quote_id: @accepted_quote.id,
-      customer_request_id: @customer_request.id
-    )
-      @customer_request.quotes.each do |quote|
-        if quote.accepted
-          CompanyMailer.accept_email(quote).deliver_now
-        else
-          CompanyMailer.decline_email(quote).deliver_now
-          @accepted_quote.update(accepted: false)
+    accepted_quote = Quote.find(params[:id])
+    customer_request = accepted_quote.customer_request
+    if current_customer.customer_requests.include?(customer_request)
+      if Contract.create(
+        quote_id: accepted_quote.id,
+        customer_request_id: customer_request.id
+      )
+        customer_request.quotes.each do |quote|
+          if quote.accepted
+            CompanyMailer.accept_email(quote).deliver_now
+          else
+            CompanyMailer.decline_email(quote).deliver_now
+            accepted_quote.update(accepted: false)
+          end
         end
+      else
+        redirect_to "/quotes"
       end
-    else
-      redirect_to "/quotes"
     end
   end
 
--- spec/controllers/contracts_controller_spec.rb
+++ spec/controllers/contracts_controller_spec.rb
@@ -12,23 +12,26 @@
 require 'rails_helper'
 
 RSpec.describe ContractsController, type: :controller do
-
-
   describe 'POST #create' do
     before :each do
       @company_one = create(:company)
       @company_two = create(:company)
       @company_three = create(:company)
-      @customer_request = create(:customer_request)
-      @quote_one = create(:quote,
+      @customer = create(:customer)
+      sign_in @customer
+      @customer_request = create(:customer_request, customer_id: @customer.id)
+      @quote_one = create(
+        :quote,
         company_id: @company_one.id,
         customer_request_id: @customer_request.id
       )
-      @quote_two = create(:quote,
+      @quote_two = create(
+        :quote,
         company_id: @company_two.id,
         customer_request_id: @customer_request.id
       )
-      @quote_three = create(:quote,
+      @quote_three = create(
+        :quote,
         company_id: @company_three.id,
         customer_request_id: @customer_request.id
       )
