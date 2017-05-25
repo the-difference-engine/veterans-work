21f6138b9e9eff6c94cc52cf444379840eae187f
Chris Woodside <christopherwoodside@gmail.com>
2017-05-25 16:37:40 -0500

    Add welcome_mailer to companyMailer

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -1,3 +1,7 @@
+require 'sendgrid-ruby'
+include SendGrid
+require 'json'
+
 class CompanyMailer < ApplicationMailer
   
   def decline_email(quote)
@@ -16,5 +20,21 @@ class CompanyMailer < ApplicationMailer
 
     mail to: @company.email,
          subject: 'Your quote was accepted by the customer! Reply to continue'
+  end
+
+  def welcome_email
+    from = Email.new(email: 'noreply@veteranswork.com')
+    subject = 'Hello World from the SendGrid Ruby Library'
+    to = Email.new(email: 'veteransworkheroku@gmail.com')
+    content = Content.new(type: 'text/plain', value: 'Welcome to Veterans Work! Your account will be evaluated before being activated!')
+    mail = Mail.new(from, subject, to, content)
+    # puts JSON.pretty_generate(mail.to_json)
+    puts mail.to_json
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
   end
 end
