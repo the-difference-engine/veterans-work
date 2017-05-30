8d256bd52120ba47cbef14d4182229f0f21ba73d
Chris Woodside <christopherwoodside@gmail.com>
2017-05-30 09:32:28 -0500

    Added MailerHelper and changed mail settings

--- app/mailers/company_mailer.rb
+++ app/mailers/company_mailer.rb
@@ -3,15 +3,14 @@ include SendGrid
 require 'json'
 
 class CompanyMailer < ApplicationMailer
+  helper MailerHelper
   
   def decline_email(quote)
     @quote = quote
     @company = quote.company
 
-    mail to: @company.email,
-         subject: 'Your quote was rejected by the customer.'
-    mail to: @company.email,
-         subject: "Your quote was rejected by the customer"
+    mail to: mail.to,
+         subject: mail.subject
   end
 
   def accept_email(quote)
@@ -22,19 +21,9 @@ class CompanyMailer < ApplicationMailer
          subject: 'Your quote was accepted by the customer! Reply to continue'
   end
 
-  def welcome_email
-    from = Email.new(email: 'noreply@veteranswork.com')
-    subject = 'Hello World from the SendGrid Ruby Library'
-    to = Email.new(email: 'veteransworkheroku@gmail.com')
-    content = Content.new(type: 'text/plain', value: 'Welcome to Veterans Work! Your account will be evaluated before being activated!')
-    mail = Mail.new(from, subject, to, content)
-    # puts JSON.pretty_generate(mail.to_json)
-    puts mail.to_json
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
+  def welcome_email(company)
+    @company = company
+    @company_email = @company.email
+    mail.welcome_mailer_params
   end
 end
