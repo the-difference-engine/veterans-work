c76ef7c5012152a95820644c96f94b92fc81ec5f
James Gates <gatorjuice@gmail.com>
2017-06-05 19:57:14 -0500

    gian cluster here'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407004950) do
+ActiveRecord::Schema.define(version: 20170525233505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,10 @@ ActiveRecord::Schema.define(version: 20170407004950) do
     t.string   "state"
     t.float    "service_radius"
     t.string   "status",                 default: "Pending"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_companies_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_companies_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true, using: :btree
   end
