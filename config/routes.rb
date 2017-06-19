72972f6f86976587c84bc1e96e06fa3ea1a410b3
jgates <jgates@leapfrogonline.com>
2017-06-19 18:29:39 -0500

    disable dat stuff

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
   devise_for :admins, :skip => [:registrations]
   as :admin do
-    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'admins' => 'devise/registrations#update', :as => 'user_registration'
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
   end
 
   devise_for :customers, :controllers => { registrations: 'customers/registrations'}
