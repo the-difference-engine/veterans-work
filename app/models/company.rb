1bef102f6549284a9e1fced6db8e02bf1d8e2c71
Eric Loos <ericloos00@gmail.com>
2017-06-23 09:24:13 -0500

    added zip code and phone number validation to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -100,7 +100,15 @@ class Company < ApplicationRecord
   has_many :customers, through: :reviews
   has_many :quotes
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true,uniqueness: {message: "is required"}
+  validates :phone, :presence => {:message => 'Phone number format is not correct'}, 
+                    :numericality => true, 
+                    :length => { :minimum => 10, :maximum => 15 }
+
+  validates_format_of :zip_code,
+                   with: /\A\d{5}-\d{4}|\A\d{5}\z/,
+                   message: "should be 12345 or 12345-1234",
+                   allow_blank: true
 
   geocoded_by :full_street_address
   after_validation :geocode
