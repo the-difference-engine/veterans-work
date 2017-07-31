6b8313641e86b491bbf99dcd6d9aa612eec012bf
James Gates <gatorjuice@gmail.com>
2017-07-31 17:45:03 -0500

    well there's some work here

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "the signin process", :type => :feature do
-  before :each do
-    create :customer, email: 'user@example.com', password: 'password'
+RSpec.describe "the signout process", :type => :feature do
+  it 'signs me out' do
+    with_customer_signed_in
+    visit "/customers/#{@customer.id}"
+    find(:css, 'ul.nav.navbar-nav.navbar-right').click_on("Logout #{@customer.email}")
+    expect(page).to have_content 'Signed out successfully'
   end
+end
 
+RSpec.describe "the signin process", :type => :feature do
   it "signs me in" do
-    visit '/customers/sign_in'
-    within('.login-form') do
-      fill_in 'customer[email]', with: 'user@example.com'
-      fill_in 'customer[password]', with: 'password'
-    end
-    click_button 'Log in'
+    with_customer_signed_in
     expect(page).to have_content 'Signed in successfully.'
   end
+end
+
+def with_customer_signed_in
+  @customer = create :customer, email: 'customer@example.com', password: 'password'
+  visit '/customers/sign_in'
+  within(".login-form") do
+    fill_in 'customer[email]', with: 'customer@example.com'
+    fill_in 'customer[password]', with: 'password'
+  end
+  click_button 'Log in'
 end
