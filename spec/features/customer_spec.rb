100542641ae63d1331fb986e3446a30f149f6bf5
Eric Loos <ericloos00@gmail.com>
2017-07-31 20:15:33 -0500

    added modal to confirm the decline of a quote and created feature test for it

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -45,3 +45,53 @@ def with_customer_signed_in
   end
   click_button 'Log in'
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+RSpec.describe "customer rejects quote", :type => :feature do
+  before :each do
+    @company = create :company, email: 'user@example.com', password: 'password', status: 'Active', service_radius: 30.0, latitude: 41.9687556, longitude: -87.6939721
+    @service_category = create :service_category, name: 'Paint'
+    @company_service = create :company_service, service_category_id: @service_category.id, company_id: @company.id
+    @customer = create :customer, email: 'customer@example.com', password: 'password'
+    @customer_request = create :customer_request, customer_id: @customer.id, service_category_id: @service_category.id, zipcode: '60622', expires_date: Date.today() + 3, latitude: 41.9687556, longitude: -87.6939721
+    @quote = create :quote, customer_request_id: @customer_request.id
+    visit '/customers/sign_in'
+    within(".login-form") do
+      fill_in 'customer[email]', with: 'customer@example.com'
+      fill_in 'customer[password]', with: 'password'
+    end
+    click_button 'Log in'
+  end
+
+  it 'customer rejects quote' do
+    find(:css, 'ul.nav.navbar-nav.navbar-right').click_on('View Quotes')
+    click_link 'Details'
+    click_button "Decline Quote"
+    click_button "Yes"
+    expect(@company.quotes.length).to eq(0)
+  end
+end
