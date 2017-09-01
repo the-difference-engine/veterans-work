494171ce4bffd5111647a2ed6caae2e20caa67f6
Erika Quinn <quinnerika16@gmail.com>
2017-08-31 19:56:23 -0500

    Create two tables for active-completed contracts

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 20170829010958) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.integer "balance"
     t.index ["confirmation_token"], name: "index_companies_on_confirmation_token", unique: true
     t.index ["email"], name: "index_companies_on_email", unique: true
     t.index ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true
@@ -78,6 +77,8 @@ ActiveRecord::Schema.define(version: 20170829010958) do
     t.integer "company_id"
     t.integer "role_id"
     t.index ["company_id", "role_id"], name: "index_companies_roles_on_company_id_and_role_id"
+    t.index ["company_id"], name: "index_companies_roles_on_company_id"
+    t.index ["role_id"], name: "index_companies_roles_on_role_id"
   end
 
   create_table "company_services", id: :serial, force: :cascade do |t|
@@ -159,10 +160,11 @@ ActiveRecord::Schema.define(version: 20170829010958) do
     t.string "name"
     t.string "resource_type"
     t.integer "resource_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
   create_table "service_categories", id: :serial, force: :cascade do |t|
