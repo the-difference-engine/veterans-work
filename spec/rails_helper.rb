09b416378997fb51839844750db3ae47bdc66e52
James Gates <gatorjuice@gmail.com>
2017-09-09 11:40:12 -0500

    mmmm working

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,40 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
+require 'devise'
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'simplecov'
+require 'rspec/rails'
+require 'devise'
+require 'capybara/rspec'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+# then, whenever you need to clean the DB
+DatabaseCleaner.clean
+
+SimpleCov.start 'rails' do
+  minimum_coverage_by_file 80
+  add_filter '/vendor/'
+  add_filter '/channels/'
+  add_filter '/controllers/companies/'
+  add_filter '/spec/'
+  add_filter '/mailers/'
+  add_filter '/helpers/'
+  add_filter '/jobs/'
+  add_filter '/models/ability.rb'
+  add_filter '/models/company.rb'
+  add_filter '/models/customer.rb'
+  add_filter '/models/application_record.rb'
+  add_filter '/controllers/companies/'
+  add_filter '/controllers/customers/'
+  add_filter '/models/contract.rb'
+  add_filter '/models/admin.rb'
+  add_filter '/models/role.rb'
+  add_filter '/lib/tasks/*'
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -27,6 +60,8 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
