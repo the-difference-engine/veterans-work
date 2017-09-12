6f388b8640c280ebce1d5378ae691e757ff4a23e
James Gates <gatorjuice@gmail.com>
2017-09-11 19:45:03 -0500

    required spec_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,7 @@ require 'devise'
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'database_cleaner'
+require 'spec_helper'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
