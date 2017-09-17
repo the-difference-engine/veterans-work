c7ebdda59ce79da34158d21d5e60536e3e0f57ba
James Gates <gatorjuice@gmail.com>
2017-09-17 12:48:00 -0500

    order controller done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,56 @@
 class OrdersController < ApplicationController
+  def index
+    if current_company
+      @orders = current_company.orders
+    elsif current_admin
+      @orders = Order.all
+    end
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:success] = 'Your order has been successfully processed.'
+      redirect_to "/orders/#{@order.id}"
+    else
+      flash[:danger] = 'Something went wrong please submit again.'
+      render :new
+    end
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:success] = 'Your order has been successfully updated.'
+      redirect_to "/orders/#{@order.id}"
+    else
+      flash[:danger] = 'Something went wrong please submit again.'
+      render :edit
+    end
+  end
+
+  def destroy
+    Order.find(params[:id]).destroy
+    redirect_to '/orders'
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :quantity
+    ).merge(company_id: current_company.id)
+  end
 end
