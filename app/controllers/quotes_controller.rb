8a2663d85e2a90446660ab9c58535c4763f6b80b
Melissa Gonzalez <marvelissa@gmail.com>
2017-10-09 18:44:59 -0500

    Fixed open quote bug and removed CR header from customer view of quote index

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,13 +1,9 @@
 class QuotesController < ApplicationController
   def index
     if current_customer
-      # if params[:request_id]
-      #   @customer_requests = CustomerRequest.where('id = ?', params[:request_id]).where('expires_date >= ?', Date.today())
-      # else
-        @customer_requests = current_customer.customer_requests.where(
-          'expires_date >= ?', Date.today()
-        ).select { |cr| cr.contract.nil? }
-      # end
+      @customer_requests = current_customer.customer_requests.where(
+        'expires_date >= ?', Date.today()
+      ).select { |cr| cr.contract.nil? }
       @open_quotes = current_customer.open_quotes
       @accepted_quotes = current_customer.accepted_quotes
     elsif current_company
