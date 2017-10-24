2b84aa8d95b5ef9c24df4e96b92e8a37f73a8108
derekj.williams <jogamer130@gmail.com>
2017-10-23 20:28:02 -0500

    in the middle of adding pop modal for completed contracts

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
-  var startDate = document.getElementById('quote_start_date');
-  var endDate = document.getElementById('quote_completion_date_estimate');
+  // var startDate = document.getElementById('quote_start_date');
+  // var endDate = document.getElementById('quote_completion_date_estimate');
 
-  startDate.addEventListener('change', function() {
-    endDate.min = startDate.value;
-  })
+  // startDate.addEventListener('change', function() {
+  //   endDate.min = startDate.value;
+  // })
 
   // Get the modal
-  var modal = document.getElementById('declined');
+  var declineModal = document.getElementById('declined');
+
+  var completedModal = document.getElementById('declined');
 
   // Get the button that opens the modal
-  var btn = document.getElementById("declinedBtn");
+  var declinedBtn = document.getElementById("declinedBtn");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks on the button, open the modal 
-  btn.onclick = function() {
-    modal.style.display = "block";
+  declinedBtn.onclick = function() {
+    declineModal.style.display = "block";
   }
 
   // When the user clicks on <span> (x), close the modal
