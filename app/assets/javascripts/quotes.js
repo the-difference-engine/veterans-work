1dae014b0a53d026320beaf26f595798fce824d8
Melissa Gonzalez <marvelissa@gmail.com>
2017-10-27 13:11:03 -0500

    Get Declined & Completed Quotes modal buttons to work

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -8,32 +8,41 @@ $(document).ready(function() {
   // Broken code
 
   // Get the modal
-  var declineModal = document.getElementById('declined');
-
-  var completedModal = document.getElementById('declined');
+  var declinedModal = document.getElementById('declined');
+  var completedModal = document.getElementById('completed');
 
   // Get the button that opens the modal
   var declinedBtn = document.getElementById("declinedBtn");
+  var completedBtn = document.getElementById("completedBtn");
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  var declinedSpan = document.getElementsByClassName("close")[0];
+  var completedSpan = document.getElementsByClassName("close")[1];
 
   // When the user clicks on the button, open the modal 
   declinedBtn.onclick = function() {
-    declineModal.style.display = "block";
-  }
+    declinedModal.style.display = "block";
+  };
+  completedBtn.onclick = function() {
+    completedModal.style.display = "block";
+  };
 
   // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  declinedSpan.onclick = function() {
+    declinedModal.style.display = "none";
+  };
+  completedSpan.onclick = function() {
+    completedModal.style.display = "none";
+  };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-    if (event.target == modal) {
-       modal.style.display = "none";
+    if (event.target == declinedModal) {
+      declinedModal.style.display = "none";
+    } else if (event.target == completedModal) {
+      completedModal.style.display = "none";
     }
-  }
+  };
 
 });
 
