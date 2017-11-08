723e18383313a6e80495aeecbc269524340f3c69
Melissa Gonzalez <marvelissa@gmail.com>
2017-11-08 12:37:00 -0600

    Get existing tests to pass & add overlay to page when sidenav is open

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,7 +1,17 @@
 function openNav() {
-  var sideNav = document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+  var overlays = document.getElementsByClassName("overlay");
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].style.backgroundColor = "rgba(0,0,0,0.5)";
+  }
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  var overlays = document.getElementsByClassName("overlay");
+  for (var i = 0; i < overlays.length; i++) {
+    overlays[i].style.backgroundColor = "transparent";
+  }
 }
+
+
