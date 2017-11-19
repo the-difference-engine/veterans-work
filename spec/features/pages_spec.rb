fa035c15a740eaf527012bf4b686b4f389a52f5a
Melissa Gonzalez <marvelissa@gmail.com>
2017-11-19 10:19:20 -0600

    Tweak navbar feature tests to differentiate between navbar links and same links on body of page

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -4,43 +4,57 @@ RSpec.describe 'Clicking navbar links', :type => :feature do
 
   it 'goes to the homepage' do
     visit '/about'
-    find_link('Home').click
+    within('.navbar') do
+      find_link('Home').click
+    end
     expect(page).to have_content 'Connect Customers in Need with Veterans in Skilled Trades'
   end
 
   it 'goes to the how page' do
     visit '/'
-    find_link('How').click
+    within('.navbar') do
+      find_link('How').click
+    end
     expect(page).to have_content 'How it Works'
   end
 
   it 'goes to the about page' do
     visit '/'
-    find_link('About').click
+    within('.navbar') do
+      find_link('About').click
+    end
     expect(page).to have_content 'For our beloved veterans in the skilled trades,'
   end
 
   it 'goes to the Customer Sign-up page' do
     visit '/'
-    page.find('#customer-sign-up').click
+    within('.navbar') do
+      page.find('#customer-sign-up').click
+    end
     expect(page).to have_content 'Customer Registration'
   end
 
   it 'goes to the Customer Sign-in page' do
     visit '/'
-    page.find('#customer-sign-in').click
+    within('.navbar') do
+      page.find('#customer-sign-in').click
+    end
     expect(page).to have_content 'Customer Sign In'
   end
 
   it 'goes to the Company Sign-in page' do
     visit '/'
-    page.find('#company-sign-in').click
+    within('.navbar') do
+      page.find('#company-sign-in').click
+    end
     expect(page).to have_content 'Company Sign In'
   end
 
   it 'goes to the Company Sign-up page' do
     visit '/'
-    page.find('#company-sign-up').click
+    within('.navbar') do
+      page.find('#company-sign-up').click
+    end
     expect(page).to have_content 'Company Registration'
   end
 end
