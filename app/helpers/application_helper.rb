f80b92e6b3826e1b436a7b33bcc2fbb8c07dc4f4
acolletti21 <ashleymcolletti@gmail.com>
2017-12-07 19:52:34 -0600

    changed nav links style if active page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
-  
-  def current_url
-    url_for :only_path=>true
-  end
-
   def is_active?(page_name)
-  "active" if params[:action] == page_name
+    if params[:action] == page_name
+      "active-nav"
+    end
   end
 end
