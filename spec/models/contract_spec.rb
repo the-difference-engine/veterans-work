2dbb7a0f3b3ad02eda10e145fcfe7d0efd741ad3
derekj.williams <jogamer130@gmail.com>
2017-12-11 18:54:12 -0600

    test is broken/buggy

--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe Contract, type: :model do
       expect(build(:contract)).to be_valid
     end
     it 'should give an error when saving quoteless contract' do
-      record = Contract.new
-      # record.quote_id = nil
-      record.save
+      contract1 = create( quote = true
+      )
+      expect(contract1).to_not be_valid
     end
   end
 end
