1340a74d7ad6981367ef9d496469ffbbd8530bd6
Tejas Rawal <trawal90@gmail.com>
2018-01-24 20:29:03 -0600

    Updated site contact information. Updated seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,48 @@
-Admin.create!([
-  {email: "admin@gmail.com", password: 'password'}
-])
-Customer.create!([
-  {email: "customer@gmail.com", password: 'password', confirmed_at: Date.today()}
-])
-Company.create!([
-  {email: "company@gmail.com", password: 'password', name: 'company1', address: "301 N Michigan Ave", city: "Chicago", state: "IL", zip_code: "60601", service_radius: 30.0, description: 'hello there handy service', phone: '3123231234', url: 'http://localhost:3000/customer_requests', status: 'Active', confirmed_at: Date.today()},
-  {email: "company2@gmail.com", password: 'password', name: 'company2', address: "2000 N Western Ave", city: "Chicago", state: "IL", zip_code: "60625", service_radius: 30.0, description: 'hello there handy service', phone: '3123231234', url: 'http://localhost:3000/customer_requests', status: 'Active', confirmed_at: Date.today()},
-  {email: "company3@gmail.com", password: 'password', name: 'company3', address: "2600 Lawrence ave", city: "Chicago", state: "IL", zip_code: "60625", service_radius: 30.0, description: 'hello there handy service', phone: '3123231234', url: 'http://localhost:3000/customer_requests'}
-])
-ServiceCategory.create!([
-  {name: "Paint"},
-  {name: "Plumbing"},
-  {name: "Carpentry"},
-  {name: "Mechanical"},
-  {name: "Electrical"}
-])
+admin = Admin.create!({email: "admin@gmail.com", password: 'password'})
+
+customer = Customer.create!({email: "customer@gmail.com", password: 'password', confirmed_at: Date.today()})
+
+company1 = Company.create!({email: "company@gmail.com", password: 'password', name: 'company1', address: "301 N Michigan Ave", city: "Chicago", state: "IL", zip_code: "60601", service_radius: 30.0, description: 'hello there handy service', phone: '3123231234', url: 'http://localhost:3000/customer_requests', status: 'Active', confirmed_at: Date.today()})
+company2 = Company.create!({email: "company2@gmail.com", password: 'password', name: 'company2', address: "2000 N Western Ave", city: "Chicago", state: "IL", zip_code: "60625", service_radius: 30.0, description: 'hello there handy service', phone: '3123231234', url: 'http://localhost:3000/customer_requests', status: 'Active', confirmed_at: Date.today()})
+company3 = Company.create!({email: "company3@gmail.com", password: 'password', name: 'company3', address: "2600 Lawrence ave", city: "Chicago", state: "IL", zip_code: "60625", service_radius: 30.0, description: 'hello there handy service', phone: '3123231234', url: 'http://localhost:3000/customer_requests'})
+
+service1 = ServiceCategory.create!({name: "Paint"})
+service2 = ServiceCategory.create!({name: "Plumbing"})
+service3 = ServiceCategory.create!({name: "Carpentry"})
+service4 = ServiceCategory.create!({name: "Mechanical"})
+service5 = ServiceCategory.create!({name: "Electrical"})
+
 CompanyService.create!([
-  {company_id: 1, service_category_id: 1},
-  {company_id: 1, service_category_id: 2},
-  {company_id: 1, service_category_id: 3},
-  {company_id: 1, service_category_id: 4},
-  {company_id: 1, service_category_id: 5},
-  {company_id: 2, service_category_id: 2},
-  {company_id: 2, service_category_id: 1},
-  {company_id: 2, service_category_id: 2},
-  {company_id: 2, service_category_id: 3},
-  {company_id: 2, service_category_id: 4},
-  {company_id: 2, service_category_id: 5},
-  {company_id: 3, service_category_id: 1},
-  {company_id: 3, service_category_id: 2},
-  {company_id: 3, service_category_id: 3},
-  {company_id: 3, service_category_id: 4},
-  {company_id: 3, service_category_id: 5},
+  {company_id: company1.id, service_category_id: service1.id},
+  {company_id: company1.id, service_category_id: service2.id},
+  {company_id: company1.id, service_category_id: service3.id},
+  {company_id: company1.id, service_category_id: service4.id},
+  {company_id: company1.id, service_category_id: service5.id},
+  {company_id: company2.id, service_category_id: service2.id},
+  {company_id: company2.id, service_category_id: service1.id},
+  {company_id: company2.id, service_category_id: service2.id},
+  {company_id: company2.id, service_category_id: service3.id},
+  {company_id: company2.id, service_category_id: service4.id},
+  {company_id: company2.id, service_category_id: service5.id},
+  {company_id: company3.id, service_category_id: service1.id},
+  {company_id: company3.id, service_category_id: service2.id},
+  {company_id: company3.id, service_category_id: service3.id},
+  {company_id: company3.id, service_category_id: service4.id},
+  {company_id: company3.id, service_category_id: service5.id},
 ])
-CustomerRequest.create!([
-  {address: "4800 N Washtenaw", city: "Chicago", state: "IL", zipcode: "60625", service_category_id: 1, description: "I need a TV mounted in my living room.", customer_id: 1, expires_date: "2021-12-31"}
-])
+
+customer_request = CustomerRequest.create!({address: "4800 N Washtenaw", city: "Chicago", state: "IL", zipcode: "60625", service_category_id: service1.id, description: "I need a TV mounted in my living room.", customer_id: customer.id, expires_date: "2021-12-31"})
+
 Quote.create!([
-  {customer_request_id: 1, company_id: 1, materials_cost_estimate: "122.0", labor_cost_estimate: "123.0", start_date: "2017-12-28", completion_date_estimate: "2017-12-31", notes: "asdfasdfsalkdflksd", accepted: nil, customer_viewed: false}
+  {
+    customer_request_id: customer_request.id,
+    company_id: company1.id,
+    materials_cost_estimate: "122.0",
+    labor_cost_estimate: "123.0",
+    start_date: "2017-12-28",
+    completion_date_estimate: "2017-12-31",
+    notes: "asdfasdfsalkdflksd",
+    accepted: nil,
+    customer_viewed: false
+  }
 ])
